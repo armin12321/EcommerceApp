19327bbff9ad187281f0083a0e4531d04f2913b3
Petrović Armin <arminpeta@gmail.com>
2020-09-20 22:52:30 +0200

    spremno za deployment za heroku

--- Frontend/src/app/services/server.service.ts
+++ Frontend/src/app/services/server.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 
 export class ServerService {
   
-  private serverURL: string = 'http://localhost:5000';
+  private serverURL: string = '';
 
   constructor(
     private http: HttpClient
@@ -16,85 +16,85 @@ export class ServerService {
 
   changeOnlineStatus(data): Observable<any> {
     // throw new Error('Method not implemented.');
-    return this.http.post(`${this.serverURL}/api/info/changeOnlineStatus`, data);
+    return this.http.post(`${this.serverURL}api/info/changeOnlineStatus`, data);
   }
 
   getRecentChats(): Observable<any> {
     // throw new Error('Method not implemented.');
-    return this.http.get(`${this.serverURL}/api/info/recentChats`);
+    return this.http.get(`${this.serverURL}api/info/recentChats`);
   }
 
   findByID(data): Observable<any> {
     // throw new Error('Method not implemented.');
-    return this.http.post(`${this.serverURL}/api/user/getByID`, data);
+    return this.http.post(`${this.serverURL}api/user/getByID`, data);
   }
   getMessagesInfo(): Observable<any> {
     // throw new Error('Method not implemented.');
-    return this.http.get(`${this.serverURL}/api/info/newMessages`);
+    return this.http.get(`${this.serverURL}api/info/newMessages`);
   }
 
   getNotifications() {
     // throw new Error('Method not implemented.');
-    return this.http.get(`${this.serverURL}/api/user/notifications`);
+    return this.http.get(`${this.serverURL}api/user/notifications`);
   }
 
   getHomeData(): Observable<any> {
-    return this.http.get(`${this.serverURL}/api/public/home`, {}); //here goes http options.
+    return this.http.get(`${this.serverURL}api/public/home`, {}); //here goes http options.
   }
 
   postRegisterData(data): Observable<any> {    
-    return this.http.post(`${this.serverURL}/api/user/register`, data);
+    return this.http.post(`${this.serverURL}api/user/register`, data);
   }
 
   postProductData(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/product/add`, data);
+    return this.http.post(`${this.serverURL}api/product/add`, data);
   }
 
   postLoginData(data): Observable<any> {    
-    return this.http.post(`${this.serverURL}/api/user/login`, data);
+    return this.http.post(`${this.serverURL}api/user/login`, data);
   }
 
   getProductPicture(data): Observable<any> {      
-    return this.http.post(`${this.serverURL}/api/product/productImage`, data, {responseType: "blob"});
+    return this.http.post(`${this.serverURL}api/product/productImage`, data, {responseType: "blob"});
   }
 
   getProfileData(): Observable<any> {
-    return this.http.get(`${this.serverURL}/api/user/profile`);
+    return this.http.get(`${this.serverURL}api/user/profile`);
   }
 
   getCartData(): Observable<any> {
-    return this.http.get(`${this.serverURL}/api/user/cart`);
+    return this.http.get(`${this.serverURL}api/user/cart`);
   }
 
   getMyProducts(): Observable<any> {
-    return this.http.get(`${this.serverURL}/api/user/products`);
+    return this.http.get(`${this.serverURL}api/user/products`);
   }
 
   getSellerData(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/public/sellerInfo`, data);
+    return this.http.post(`${this.serverURL}api/public/sellerInfo`, data);
   }
 
   getProductData(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/public/productInfo`, data);
+    return this.http.post(`${this.serverURL}api/public/productInfo`, data);
   } 
 
   getAvatarImage(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/public/avatarImage`, data, {responseType: "blob"});
+    return this.http.post(`${this.serverURL}api/public/avatarImage`, data, {responseType: "blob"});
   }
 
   getTopProducts(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/public/topProducts`, data);
+    return this.http.post(`${this.serverURL}api/public/topProducts`, data);
   }
 
   loadOldMessages(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/user/chat/loadMessages`, data);
+    return this.http.post(`${this.serverURL}api/user/chat/loadMessages`, data);
   }
 
   sendMessagee(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/user/chat/sendMessage`, data);
+    return this.http.post(`${this.serverURL}api/user/chat/sendMessage`, data);
   }
 
   getNewMessages(data): Observable<any> {
-    return this.http.post(`${this.serverURL}/api/user/chat/getNewMessages`, data);
+    return this.http.post(`${this.serverURL}api/user/chat/getNewMessages`, data);
   }
 }
